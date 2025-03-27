e82c7ea6ba53bc5bf44ca307ee1d59f8ef8cb773
luigi-giordano <luigi.giordano1992@gmail.com>
2025-03-27 19:57:05 +0100

    [bonus 2]

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 async function getChefBirthday(id) {
+
+  let ricetta;
+  let chef;
+
   try {
     // Recupero la ricetta
     const rispostaRicetta = await fetch(`https://dummyjson.com/recipes/${id}`);
-    const ricetta = await rispostaRicetta.json();
-    if (!ricetta.userId) throw new Error("ID utente non trovato nella ricetta");
+    ricetta = await rispostaRicetta.json();
+    if (ricetta.message) throw new Error("ID utente non trovato nella ricetta");
 
     // Estrazione userId dalla ricetta
     const userId = ricetta.userId;
 
     // Recupero le informazioni dello chef
     const chefResponse = await fetch(`https://dummyjson.com/users/${userId}`);
-    const chef = await chefResponse.json();
-    if (!chef.birthDate) throw new Error("Data di nascita non trovata per lo chef");
+    chef = await chefResponse.json();
+    if (chef.message) throw new Error("Data di nascita non trovata per lo chef");
+    const formattedData = dayjs(chef.birthDate).format('DD/MM/YYYY')
 
     // Restituisco la data di nascita dello chef
-    return chef.birthDate;
+    return formattedData;
   } catch (error) {
     console.error(error.message);
-    throw new Error("Errore nel recupero dei dati: ", error.message);
+    throw new Error("Errore nel recupero dei dati: " + error.message);
   }
 }
 
