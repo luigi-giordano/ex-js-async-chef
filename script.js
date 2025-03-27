b1e890d50b6f1486c36abf5b9d877b48effcc057
luigi-giordano <luigi.giordano1992@gmail.com>
2025-03-27 20:05:50 +0100

    [fix error messagge in fetch][bonus 1 not completed]

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 async function getChefBirthday(id) {
-
   let ricetta;
   let chef;
 
@@ -7,7 +6,10 @@ async function getChefBirthday(id) {
     // Recupero la ricetta
     const rispostaRicetta = await fetch(`https://dummyjson.com/recipes/${id}`);
     ricetta = await rispostaRicetta.json();
-    if (ricetta.message) throw new Error("ID utente non trovato nella ricetta");
+    if (ricetta.message) {
+      console.error("ID utente non trovato nella ricetta");
+      throw new Error("ID utente non trovato nella ricetta");
+    }
 
     // Estrazione userId dalla ricetta
     const userId = ricetta.userId;
@@ -15,8 +17,12 @@ async function getChefBirthday(id) {
     // Recupero le informazioni dello chef
     const chefResponse = await fetch(`https://dummyjson.com/users/${userId}`);
     chef = await chefResponse.json();
-    if (chef.message) throw new Error("Data di nascita non trovata per lo chef");
-    const formattedData = dayjs(chef.birthDate).format('DD/MM/YYYY')
+    if (chef.message) {
+      console.error("Data di nascita non trovata per lo chef");
+      throw new Error("Data di nascita non trovata per lo chef");
+    }
+
+    const formattedData = dayjs(chef.birthDate).format('DD/MM/YYYY');
 
     // Restituisco la data di nascita dello chef
     return formattedData;
@@ -26,6 +32,12 @@ async function getChefBirthday(id) {
   }
 }
 
-getChefBirthday(1)
-  .then(birthDate => console.log("Data di nascita dello chef:", birthDate))
-  .catch(error => console.log("Errore:", error.message));
+(async () => {
+  try {
+    const birthDate = await getChefBirthday(1);
+    console.log("Data di nascita dello chef:", birthDate);
+  } catch (error) {
+    console.log("Errore:", error.message);
+  }
+})();
+
